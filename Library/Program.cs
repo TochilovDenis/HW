c282c473187706a5accaffab7d9ab880f956a4d0
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-25 21:01:02 +0300

    Подключение к БД (DbConnection)  Базовая реализация подключения к базе данных в библиотеке с использованием ADO.NET.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,16 @@ namespace Library
 {
     class Program
     {
+        public Program()
+        {
+            SqlConnection conn = null;
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\v11.0; Initial Catalog=Library; Integrated Security=SSPI;";
+            // или
+            //SqlConnection conn = null;
+            //conn = new SqlConnection(@"Data Source=(localdb)\v11.0; Initial Catalog=Library; Integrated Security=SSPI;");
+        }
+
         static void Main(string[] args)
         {
 
