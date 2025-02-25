48c6519fe7b92fa14f2a92e18ad92d0e56c32af7
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-25 20:27:29 +0300

    Добавлена функция просмотра изображений с поддержкой буфера обмена
    Добавлен просмотрщик изображений с двумя подходами к загрузке изображений из буфера обмена:
    - Простой подход: прямая загрузка URL без валидации
    - Безопасный подход: валидация URL и обработка ошибок
    
    Включает:
    - Проверку протоколов http/https
    - Обработку ошибок для некорректных URL
    - Обратную связь с пользователем через сообщения
    - Режим масштабирования для отображения изображения

--- image_viewer/Form1.Designer.cs
+++ image_viewer/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(804, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Простой способ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.BtnSimple_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(545, 639);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(593, 64);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(545, 639);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(12, 18);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(786, 35);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Location = new System.Drawing.Point(972, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Безопасный способ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.BtnSafe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1163, 723);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- image_viewer/Form1.cs
+++ image_viewer/Form1.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace image_viewer
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            // Настройка PictureBox для масштабирования изображения
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                       
+            // Подписываемся на события нажатия кнопок
+            button1.Click += BtnSimple_Click;
+            button2.Click += BtnSafe_Click;
+        }
+
+        private void BtnSimple_Click(object sender, EventArgs e)
+        {
+            // Простой способ: прямая загрузка без проверок
+            pictureBox1.Load(Clipboard.GetText());
+            webBrowser1.Navigate(Clipboard.GetText());
+        }
+
+        private void BtnSafe_Click(object sender, EventArgs e)
+        {
+            // Безопасный способ: с проверками и обработкой ошибок
+            try
+            {
+                string url = Clipboard.GetText();
+
+                if (!string.IsNullOrEmpty(url) &&
+                    (url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ||
+                     url.StartsWith("https://", StringComparison.OrdinalIgnoreCase)))
+                {
+                    pictureBox1.Load(url);
+                    webBrowser1.Navigate(url);
+                }
+                else
+                {
+                    MessageBox.Show("В буфере обмена нет корректного URL адреса");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке: {ex.Message}");
+            }
         }
     }
 }
