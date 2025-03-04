343a290343887d5767b9263a62ff880b7af6cb75
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-04 12:55:41 +0300

    ReactionTimeGame 1. Измерение времени:     - Используется класс Stopwatch для точного измерения времени реакции     - StartNew() запускает отсчёт времени     - ElapsedMilliseconds возвращает время в миллисекундах 2. Обработка ввода:     - Console.ReadKey(true) ожидает нажатия клавиши без отображения символа     - Второй параметр true предотвращает вывод символа в консоль 3. Цикл игры:     - Игра повторяется до тех пор, пока пользователь хочет продолжать     - После каждого раунда предлагается сыграть снова

--- ReactionTimeGame/Program.cs
+++ ReactionTimeGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,43 @@ namespace ReactionTimeGame
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Добро пожаловать в игру \"Успел, не успел\"!");
+            Console.WriteLine("Когда появится сигнал, нажмите любую клавишу как можно быстрее.");
+
+            Random random = new Random();
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                Console.WriteLine("\nЖдите сигнал...");
+
+                // Случайная задержка от 1 до 5 секунд
+                int delay = random.Next(1000, 5000);
+                System.Threading.Thread.Sleep(delay);
+
+                Console.WriteLine("Нажмите любую клавишу!");
+
+                // Запускаем измерение времени
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                // Ждем нажатия клавиши
+                Console.ReadKey(true);
+
+                // Останавливаем измерение
+                stopwatch.Stop();
+
+                // Получаем время реакции в миллисекундах
+                long reactionTime = stopwatch.ElapsedMilliseconds;
+
+                Console.WriteLine($"\nВаше время реакции: {reactionTime} миллисекунд");
+
+                Console.Write("\nХотите сыграть еще раз? (да/нет): ");
+                string answer = Console.ReadLine().ToLower();
+                playAgain = answer == "да";
+            }
+
+            Console.WriteLine("Спасибо за игру!");
         }
     }
 }
