87c8f97f11d756e2b5ffc0bb331a414b277d21e1
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-04 14:41:32 +0300

    Реализация последовательного алгоритма поиска простых чисел
    Почему:
    - Последовательная обработка обеспечивает базовую реализацию для сравнения производительности
    - Метод использует ту же оптимизацию через начало проверки с квадрата числа
    - Встроенный таймер позволяет измерять производительность и сравнивать с параллельной версией
    
    Что сделано:
    - Реализован метод FindPrimesSequential для последовательного поиска простых чисел
    - Использован обычный цикл for вместо Parallel.For
    - Добавлена оптимизация через начало проверки делящих с квадрата числа
    - Внедрен измеритель времени для оценки производительности
    
    Изменения в коде:
    - Создан массив флагов isPrime размером maxNumber + 1
    - Реализована последовательная обработка через обычный цикл for
    - Оптимизирована проверка множителей
    - Добавлено измерение времени выполнения

---
+++ Number_Parallel/PrimeFinderSequential.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Parallel
+{
+    // Последовательный поиск простых чисел с обычным For
+    class PrimeFinderSequential
+    {
+        public static void FindPrimesSequential(int maxNumber)
+        {
+            var sw = Stopwatch.StartNew();
+
+            bool[] isPrime = new bool[maxNumber + 1];
+            for (int i = 0; i <= maxNumber; i++)
+                isPrime[i] = true;
+
+            for (int x = 2; x <= maxNumber; x++)
+            {
+                if (isPrime[x])
+                {
+                    int square = x * x;
+                    for (int i = square; i <= maxNumber; i += x)
+                        isPrime[i] = false;
+                }
+            }
+
+            Console.WriteLine($"Последовательный метод выполнился за {sw.ElapsedMilliseconds} мс");
+        }
+    }
+}
--- Number_Parallel/Program.cs
+++ Number_Parallel/Program.cs
@@ -10,13 +10,6 @@ namespace Number_Parallel
     {
         static void Main(string[] args)
         {
-            const int maxNumber = 10000;
-
-            Console.WriteLine("Начало сравнения методов:");
-            PrimeFinderParallel.FindPrimesParallel(maxNumber);
-
-            Console.WriteLine("Завершено.");
-            Console.ReadKey();
         }
     }
 }
