340f3f3f4aedcdd931eb52aaa2d730bcb512b43a
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-04 13:56:58 +0300

    Создать приложение, использующее механизм событий. Создайте в коде приложения несколько потоков. Первый поток генерирует 1000 чисел в диапазоне от 0 до 5000. Три потока ожидают, когда генерирование будет завершено. Когда все числа сгенерированы, три потока стартуют процесс анализа полученных данных. Первый поток находит максимум среди чисел. Второй поток находит минимум. Третий поток высчитывает среднее арифметическое.

---
+++ multithreading_number_generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("multithreading_number_generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("multithreading_number_generator")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("73091481-e31f-4c7a-8c22-e9b2d70dff06")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
